b0e0528519b6a335ea4aa21722bee6b84a4db235
Jino John <leojino@gmail.com>
2019-09-16 00:43:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,128 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const WSClient = new WebSocket('wss://stocks.mnet.website');
+
+class App extends Component {
+
+  state = {
+    loading: true,
+    data: []
+  }
+
+  componentDidMount() {
+    WSClient.onopen = () => {
+      setTimeout(()=> {
+        this.setState( {loading: false} )
+      },500);
+    }
+
+    WSClient.onmessage = message => {
+      const stocks =  this.createTableData(JSON.parse(message.data))
+      this.setState({data: stocks});
+    }
+  }
+
+  createTableData(stocks) {
+    const {data} = this.state;
+
+    stocks.forEach( stock => {
+
+      if( data[ stock[0] ] ){
+        const prev = Object.keys(data).find( d => d === stock[0] );
+
+        let increase = null
+          
+        if(prev) {
+          if( data[prev].price>stock[1] ) {
+            increase = true;
+          }else if (data[prev].price<stock[1] ) {
+            increase = false;
+          }
+        }
+
+        data[ stock[0] ] = {
+          ticker: stock[0],
+          price: stock[1],
+          text: stock[1].toFixed(2),
+          increase,
+          lastUpdate: new Date()
+        }
+      }else{
+        data[ stock[0] ] = {
+          ticker: stock[0],
+          price: stock[1],
+          text: stock[1].toFixed(2),
+          increase: null,
+          lastUpdate: new Date()
+        }
+      }
+    } )
+
+    return data;
+
+  }
+  
+  render() {
+    const {loading, data} = this.state;
+
+    return (
+      <div className="App">
+        <header className={`App-header ${ loading ? "" : "App-header-loading-complete" } `}>
+          <img src={logo} className={`App-logo ${ loading ? "": "logo-loading-complete" }`} alt="logo" />
+          {loading && 
+            <p>
+              Loading ...
+            </p>
+          }
+        </header>
+        {
+          !loading && data && 
+          <StocksTable stocks={data}/>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+class StocksTable extends Component {
+
+  getTDBG = increase => {
+    if( increase === null ) return "";
+    if( increase === true ) return "increase";
+    if( increase === false ) return "decrease";
+  }
+
+  render () {
+    const {stocks} = this.props;
+    const keys = Object.keys( stocks );
+    // return <div>text</div>
+
+    return (
+      <table className="pure-table">
+        <thead>
+          <tr>
+            <th>Ticker</th>
+            <th>Price</th>
+            <th>Last Updated at</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            keys.map((d, i)=> (
+              <tr key={`${i}`} >
+                <td>{ stocks[d].ticker }</td>
+                <td  className={["Stocks-price-cell", this.getTDBG( stocks[d].increase )].join(" ")} >{ stocks[d].text }</td>
+                <td> { stocks[d].lastUpdate.getTime() } </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    )
+  }
+}
