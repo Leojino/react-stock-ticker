ff70ed661ac73743962a9d925c36e37652de430e
Jino John <leojino@gmail.com>
2019-09-16 00:39:58 +0530

    Updates

---
+++ precache-manifest.e2639e7004a3e5fe5dc7415fa4e71213.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "539a86f8271c102663a81473bc4e71e8",
+    "url": "/react-stock-ticker/index.html"
+  },
+  {
+    "revision": "f2738ee3e47ace1c39f7",
+    "url": "/react-stock-ticker/static/css/main.67750fd9.chunk.css"
+  },
+  {
+    "revision": "f5cc4bd4efd11a604b13",
+    "url": "/react-stock-ticker/static/js/2.ee31819d.chunk.js"
+  },
+  {
+    "revision": "f2738ee3e47ace1c39f7",
+    "url": "/react-stock-ticker/static/js/main.0255aebd.chunk.js"
+  },
+  {
+    "revision": "f672061724fd9be690d9",
+    "url": "/react-stock-ticker/static/js/runtime~main.046c1707.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-stock-ticker/static/media/logo.5d5d9eef.svg"
+  }
+]);
